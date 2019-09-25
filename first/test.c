bbc2252deec1439bc56aaac26717dc5ab5b10c2e
isWarrens <1489070715@qq.com>
2019-09-24 21:57:37 -0700

    first

---
+++ first/test.c
@@ -0,0 +1,18 @@
+#include "ourhdr.h"
+
+char buf1[] = "abcdefghij";
+char buf2[] = "ABCDEFGHIJ";
+
+int main()
+{
+	int fd;
+	if((fd = creat("file.hole",FILE_MODE )) <0 )
+		perror("creat error");
+	if (write(fd, buf1, 10) != 10)
+		perror("buf1 write error");
+	if(lseek(fd, 40, SEEK_SET) == -1)
+		perror("lseek error");
+	if(write(fd, buf2, 10) != 10)
+		perror("buf2 write error");
+	return 0;
+}
